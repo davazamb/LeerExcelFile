727dd9fdcf9df3a85ca98e34a61bd4d6693caf82
David Ali Zambrano Burgos <davazamb10@outlook.com>
2018-07-16 13:25:07 -0400

    Agregado string de conexion para version 97-03 y 2007

--- LeerExcelFile/Program.cs
+++ LeerExcelFile/Program.cs
@@ -10,6 +10,9 @@ namespace LeerExcelFile
     {
         static void Main(string[] args)
         {
+            //string connString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\\Temp\\ArchivoExcel.xls; Extended Properties='Excel 8.0;HDR=NO;IMEX=1;'";
+            string connString = "Provider= Microsoft.ACE.OLEDB.12.0;" + "Data Source=C:\\Temp\\ArchivoExcel.xlsx" + ";Extended Properties='Excel 8.0;HDR=Yes'";
+
         }
     }
 }
